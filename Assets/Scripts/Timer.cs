484deede28a8fea30d10f16ef0acebedc6c83d29
Pastew <kpastew@gmail.com>
2017-01-20 23:24:44 +0100

    Timescale

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Timer : MonoBehaviour {
+
+    [SerializeField]
+    private int tickEverySeconds = 1;
+
+    [SerializeField]
+    private int hoursAddedEveryTick = 1;
+
+
+    private DateTime dateValue;
+    private float ticks = 0;
+
+    private TextMesh textMesh;
+
+    void Start () {
+        dateValue = new DateTime(1952, 5, 3, 12, 0, 0);
+        print(dateValue);
+        InvokeRepeating("Tick", 0, tickEverySeconds);
+
+        textMesh = GetComponent<TextMesh>();
+    }
+	
+    void Tick()
+    {
+        dateValue = dateValue.AddHours(tickEverySeconds);
+        textMesh.text = dateValue.ToString();
+        ticks++;
+        textMesh.text = dateValue.Hour + ":00\n" + dateValue.Year + "-" + dateValue.Month + "-" + dateValue.Day ;
+
+
+    }
+
+}
---
+++ Assets/Scripts/TimerMultiplierButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerMultiplierButton : MonoBehaviour {
+
+    [SerializeField]
+    private int mutliplyBy = 1;
+
+	void OnMouseDown()
+    {
+        Time.timeScale = mutliplyBy;
+        print("New time scale: " + Time.timeScale);
+    }
+}
