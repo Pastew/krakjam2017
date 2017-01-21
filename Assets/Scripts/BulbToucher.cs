09c93ca7d62cb67742a1cfb9e930d22a38fe9236
Pastew <kpastew@gmail.com>
2017-01-21 19:33:18 +0100

    Light bulb

---
+++ Assets/Scripts/BulbToucher.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulbToucher : MonoBehaviour {
+
+    NormalLightBulb bulb;
+
+    void Start()
+    {
+        bulb = FindObjectOfType<NormalLightBulb>();
+    }
+
+	void Update () {
+		transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        pos.z = bulb.transform.position.z;
+        transform.position = pos;
+
+        if (Input.GetMouseButtonUp(0))
+            transform.position = new Vector3(10000, 10000, 10000);
+    }
+}
---
+++ Assets/Scripts/NormalLightBulb.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NormalLightBulb : MonoBehaviour {
+
+	void Update () {
+		
+	}
+
+    void OnMouseDown()
+    {
+        Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 mousePos = new Vector2(pos.x, pos.y);
+        Vector2 bulbPos = transform.position;
+        Vector2 sub = bulbPos - mousePos;
+
+        float multiplier = 1000;
+        Vector2 force = sub * multiplier;
+        print("Got force: " + force);
+        GetComponent<Rigidbody2D>().AddForce(force);
+    }
+}
