1d4f63ae7bacaa6f1863b7f37bd02976698c5b03
Pastew <kpastew@gmail.com>
2017-01-21 05:12:09 +0100

    buttons

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -7,16 +7,30 @@ public class Button : MonoBehaviour {
     [SerializeField]
     private string panelToOpen;
 
+    [SerializeField]
+    private Sprite hover;
+    private Sprite standard;
+
+    SpriteRenderer sr;
 
     void Start () {
-	}
+        standard = GetComponent<SpriteRenderer>().sprite;
+        sr = GetComponent<SpriteRenderer>();
+    }
 	
 	void Update () {
 		
 	}
 
+    void OnMouseDown()
+    {
+        if(hover)
+            sr.sprite = hover;
+    }
+
     void OnMouseUp()
     {
+        sr.sprite = standard;
         if(panelToOpen == "left")
             FindObjectOfType<MoveCamera>().OpenLeftMenu();
         if (panelToOpen == "right")
