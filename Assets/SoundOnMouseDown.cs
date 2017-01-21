6bf311e7b789b9edbcb6565af5dfab21c1364703
Pastew <kpastew@gmail.com>
2017-01-21 15:18:42 +0100

    basic sounds

---
+++ Assets/SoundOnMouseDown.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class SoundOnMouseDown : MonoBehaviour {
+
+    [SerializeField]
+    AudioClip soundToPlay;
+
+    AudioSource audio;
+
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    void OnMouseDown()
+    {
+
+        audio.PlayOneShot(soundToPlay, 0.7F);
+    }
+}
