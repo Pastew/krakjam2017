14275b8d91fe0a80b67277822282b7617045a3de
Pastew <kpastew@gmail.com>
2017-01-21 03:29:38 +0100

    .

---
+++ Arek/Influence.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Sieciech on 2017-01-21.
+ */
+public class Influence {
+
+    int counter;
+    double power;
+
+    public double getInfluence(double mood){
+
+        this.counter--;
+
+        if(counter < 0) return 0;
+
+        return power;
+    }
+
+}
---
+++ Arek/main.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Sieciech on 2017-01-20.
+ */
+
+class main{
+public static void main(String [ ] args) {
+
+
+
+        }
+}
