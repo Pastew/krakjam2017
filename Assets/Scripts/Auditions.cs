baee6543d68dbf6be5d4e43d526963633881b081
Pastew <kpastew@gmail.com>
2017-01-21 13:02:01 +0100

    Scroll view works

---
+++ Assets/Scripts/Auditions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Auditions : MonoBehaviour {
+
+    List<Audition> auditions;
+    public Button auditionButtonPrefab;
+
+	void Start () {
+        AddNewAuditionToPanel(Audition.nextID++, "Jakas aydycja");
+        AddNewAuditionToPanel(Audition.nextID++, "J22akas aydycja");
+        AddNewAuditionToPanel(Audition.nextID++, "J33akas aydycja");
+        AddNewAuditionToPanel(Audition.nextID++, "Ja44kas aydycja");
+        AddNewAuditionToPanel(Audition.nextID++, "Jak55as aydycja");
+        AddNewAuditionToPanel(Audition.nextID++, "Jak55as aydycja");
+        AddNewAuditionToPanel(Audition.nextID++, "Jak55as aydycja");
+        AddNewAuditionToPanel(Audition.nextID++, "Jak55as aydycja");
+        AddNewAuditionToPanel(Audition.nextID++, "Jak55as aydycja");
+    }
+	
+	void Update () {
+		
+	}
+
+
+    public void AddNewAuditionToPanel(int id, string desc)
+    {
+        Transform parent = GetComponentInChildren<ContentSizeFitter>().gameObject.transform;
+        Button newAudition = Instantiate(auditionButtonPrefab, parent, false);
+        Audition audition = newAudition.gameObject.AddComponent<Audition>();
+        audition.id = id;
+        audition.description = desc;
+        newAudition.GetComponentInChildren<Text>().text = audition.GetDescription();
+        Button b = newAudition.GetComponent<Button>();
+        b.onClick.AddListener(() => { AuditionClicked(audition); });
+
+    }
+
+    public void AuditionClicked(Audition audition)
+    {
+        audition.OnAuditionChosen();
+    }
+}
---
+++ Assets/Scripts/Effect1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Effect1 : Effect {
+
+    public Effect1()
+    {
+        id = 1;
+        lifeTime = 10;
+        counter = 0;
+    }
+
+    public override bool evaluate(City city)
+    {
+        if (counter >= lifeTime)
+        {
+            city.removeEffect(this);
+            return false;
+        }
+
+        counter++;
+        city.setMood(10);
+        return true;
+    }
+}
