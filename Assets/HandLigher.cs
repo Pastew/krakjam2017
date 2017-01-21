bd091cda6d7602adeb034901aa4bf9500701fdd6
Pastew <kpastew@gmail.com>
2017-01-21 19:50:56 +0100

    sosadasdsa

---
+++ Assets/HandLigher.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandLigher : MonoBehaviour {
+
+    Light light;
+
+    void Start()
+    {
+        light = GetComponent<Light>();
+    }
+
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(2))
+            light.enabled = !light.enabled;
+
+    }
+}
