c0a6682c3d0737f80d8a4c829f0ae571ba1b538e
ympek <sz@ympek.net>
2017-01-22 09:32:23 +0100

    to sie buduje a tamto nei

--- Assets/Scripts/Effect0.cs
+++ Assets/Scripts/Effect0.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class Effect0 : Effect {
 
-    private float addition;
+    private double addition;
 
     public Effect0()
     {
@@ -17,20 +17,21 @@ public class Effect0 : Effect {
 
     public override void evaluate(City city)
     {
-        recalculateData();
-        
+        recalculateData(city);
+
         if (counter >= lifeTime)
         {
             city.RemoveEffect(id);
         }
 
-        city.xA += addition;
+        city.xA += (float)addition;
         counter++;
     }
 
     public override void recalculateData(City city) {
+        float temp = (float)lifeTime;
         float param = 1 + (city.population - 100000) / 225000;
-        lifeTime *= param;
+        lifeTime = (int)(param * temp);
         addition = 3 * ((11 - city.population / 100000) * 0.05 + 0.5);    
     }
 }
--- Assets/Scripts/Effect1.cs
+++ Assets/Scripts/Effect1.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class Effect1 : Effect {
 
-    private float addition;
+    private double addition;
 
     public Effect1()
     {
@@ -17,20 +17,21 @@ public class Effect1 : Effect {
 
     public override void evaluate(City city)
     {
-        recalculateData();
+        recalculateData(city);
         
         if (counter >= lifeTime)
         {
             city.RemoveEffect(id);
         }
 
-        city.xM += addition;
+        city.xM += (float)addition;
         counter++;
     }
 
     public override void recalculateData(City city) {
+        float temp = (float)lifeTime;
         float param = 1 + (city.population - 100000) / 225000;
-        lifeTime *= param;
+        lifeTime = (int)(param * temp);
         addition = 20 * ((11 - city.population / 100000) * 0.05 + 0.5);    
     }
 }
